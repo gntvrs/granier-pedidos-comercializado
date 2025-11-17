1ffb8a5df3e085afaec410d89ef12daa25d49d1d
gntvrs <132781319+gntvrs@users.noreply.github.com>
2025-11-17 14:52:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
 from fastapi import FastAPI
 from pipeline import ejecutar_pipeline
 
-app = FastAPI(
-    title="Planificación Proveedores Granier",
-    version="1.0.0"
-)
+app = FastAPI()
 
 @app.get("/planificar")
 def planificar(
     proveedor_id: int,
     consumo_extra_pct: float = 0.0
 ):
-    """
-    Ejecuta la planificación completa:
-    - Generación dinámica de filtro Centro-Material
-    - Ajuste del consumo medio
-    - Forecast 60 días
-    - Generación de pedidos
-    - Ajustes logísticos
-    - Persistencia en BigQuery
-    """
-    result = ejecutar_pipeline(
+    resultado = ejecutar_pipeline(
         proveedor_id=proveedor_id,
         consumo_extra_pct=consumo_extra_pct
     )
@@ -29,5 +17,5 @@ def planificar(
         "status": "OK",
         "proveedor_id": proveedor_id,
         "consumo_extra_pct": consumo_extra_pct,
-        "detalles": result
+        "resultado": resultado
     }
